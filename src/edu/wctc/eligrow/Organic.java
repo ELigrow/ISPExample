6afb1f895d7e25a1da6e5209b43fdede89bf8047
Elijah Ligrow <eligrow@my.wctc.edu>
2018-10-17 18:20:58 -0500

    Initial commit

---
+++ src/edu/wctc/eligrow/Organic.java
@@ -0,0 +1,11 @@
+package edu.wctc.eligrow;
+
+public interface Organic {
+
+    public void talk(String s);
+    public void run(int distance);
+    public void jump();
+    public void hike();
+    public void swim();
+    public void trip();
+}
---
+++ src/edu/wctc/eligrow/Vehicle.java
@@ -0,0 +1,9 @@
+package edu.wctc.eligrow;
+
+public interface Vehicle {
+
+    public void start();
+    public void drive(int distance);
+    public void stopByBreaking();
+
+}
